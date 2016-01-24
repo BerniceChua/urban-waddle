254d5eb4c3aeb6746715c63826a0728e598f7a99
Bernice Anne W. Chua <email": null>
2016-01-23 17:34:09 -0800

    Add Coordinate model.

---
+++ app/models/coordinate.rb
@@ -0,0 +1,10 @@
+class Coordinate < ActiveRecord::Base
+  # Remember to create a migration!
+
+  geocoded_by :full_street_address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
+
+end
